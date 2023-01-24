2118d940ab955e35a0774777719490ad5e674923
Aditya Dhanraj <pandeydhanrajaditya@gmail.com>
2023-01-24 23:42:22 +0530

    All the codes written

--- block-BJaaao/code/index.js
+++ block-BJaaao/code/index.js
@@ -16,6 +16,10 @@ let num2 = 32;
 - Alert `The value of num1 is [num1] and the value of num2 is [num2]`
 - Alert `The sum of [num1] and [num2] is [num1 + num1]`
 */
+let language="Javascript";
+alert(`I am learning ${language}`);
+alert(`The value of num1 is ${num1} and the value of num2 is ${num2}`);
+alert(`The sum of ${num1} and ${num2} is ${num1 + num2}`);
 
 /*
 2. Do the following using `alert`, `prompt` and `confirm` and `console`
@@ -25,13 +29,26 @@ let num2 = 32;
   - Using confirm check if user is adult or not and store the value in `isAdult`
   - Alert message `I am [userName] a [userProfession]`
   - Log the message `I am [userName] a [userProfession]`
-  - Alet message `Username: [userName]
+  - Alert message `Username: [userName]
     Adult: [isAdult]
     Profession: [userProfession]` (it should be in three different lines)
   - Log the message using console.log `Username: [userName]
     Adult: [isAdult]
     Profession: [userProfession]` (it should be in three different lines)
 */
+let userName=prompt(`What is your name?`);
+let userProfession=prompt(`What is your profession?`);
+let  isAdult=confirm(`Are you an adult?`);
+alert(`I am ${userName} a ${userProfession}`);
+console.log(`I am ${userName} a ${userProfession}`);
+alert(`Username: ${userName}
+Adult: ${isAdult}
+Profession: ${userProfession}`);
+console.log(`Username: ${userName}
+Adult: ${isAdult}
+Profession: ${userProfession}`);
+
+
 
 /*
 . Addition using prompt
@@ -43,3 +60,9 @@ let num2 = 32;
   - Log using console.log [numA - numB]
   - Log using console.log [numA * numB]
 */
+let numA=prompt(`Enter a number`), numB=prompt(`Enter Second Number`);
+alert(`Sum=${Number(numA)+Number(numB)}`);
+alert(`Difference=${Number(numA)-Number(numB)}`);
+alert(`Product=${Number(numA)*Number(numB)}`);
+console.log(`Difference=${Number(numA)-Number(numB)}`);
+console.log(`Product=${Number(numA)*Number(numB)}`);
