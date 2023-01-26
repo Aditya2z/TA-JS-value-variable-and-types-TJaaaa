878b7248e76b26cab07e5c9133a9c35409f71c3a
Aditya Dhanraj <pandeydhanrajaditya@gmail.com>
2023-01-26 21:44:44 +0530

    All codes written

--- block-BJaabb/code/index.js
+++ block-BJaabb/code/index.js
@@ -6,21 +6,67 @@
 4. Now change the value of `user` to `John` and log the value of both `user` and `userName`.
 */
 
+let user = prompt(`Enter Your Name`);
+alert(` ${user} `);
+
+let userName = user;
+console.log(` name is ${user} 
+    \n username is ${userName}  `);
+
+let age = 22 ,  isLearning = true;
+
+user = `John`;
+console.log(` name is ${user} 
+    \n username is ${userName}  `);
+
 /* 2.
 Use `prompt` to take two numbers from user and store it in variable `numA` and `numB`. Create a third variable called `sum`, using `+` operator add numA and numB , store it in varible `sum` and using alert display `sum` in browser.
 */
+
+let numA = Number( prompt(`Enter a number`) );
+let numB = Number( prompt(`Enter another number`) );
+let sum = numA + numB;
+alert(`Sum is ${sum} `);
 
 // Loops
 
 // 3. Complete the following code to make the output be 0 2 4 6 8 10?
 
-for (let j = 0; j <= 10; ____) console.log(j);
+  for (let j = 0; j <= 10; j++) {
+    if (j % 2 === 0) {
+        console.log(j);
+    }
+}
 
 // 4. Print the odd numbers from 9 to 1 (both inclusive) using a for loop.
 
+  for (let j = 10; j >= 0; j--) {
+    if (j % 2 !== 0) {
+        console.log(j);
+    }
+}
+
 // 5. Concat all the numbers from 5 to 0 (both inclusive) in descending order using a while loop. (543210)
+let counter = 5;
+let stringCounter , concat=``;
+  while (counter >= 0) {
+    stringCounter = String(counter);
+    concat = concat + stringCounter;
+    counter--;
+  }
+console.log(concat);
 
 // 6. Concat all the even numbers from 10 to 0 (both inclusive) in descending order using a while loop. (1086420)
+counter = 10;
+stringCounter , concat=``;
+  while (counter >= 0) {
+    stringCounter = String(counter);
+    if (counter % 2 === 0) {
+        concat = concat + stringCounter;
+    }
+    counter--;
+  }
+console.log(concat);
 
 // Comparisoin
 
@@ -36,3 +82,25 @@ Example:
 "hello", -21; // false
 
 */
+
+let num1 = prompt(`Enter a number for comparison`);
+
+  if (String(Number(num1)) === "NaN") {
+    alert(`Enter a valid value`);
+} else {
+    num1 = Number(num1);
+}
+
+let num2 = prompt(`Enter another number for comparison`);
+
+  if (String(Number(num2)) === "NaN") {
+    alert(`Enter a valid value`);
+} else {
+    num2 = Number(num2);
+}
+
+(num1 === num2) ? alert(`true`) : alert(`false`);
+
+
+
+
